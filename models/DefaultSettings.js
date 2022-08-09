277b9c6248caaf470f5ad202cb57e4950f78a0c4
Akbar <akhusanbaev1@gmail.com>
2022-08-10 00:11:52 +0500

    make it better

--- models/DefaultSettings.js
+++ models/DefaultSettings.js
@@ -3,7 +3,7 @@ const DefaultSettingsSchema = new Schema({
   freeTrialSearches: {type: Number},
   vipDailyPrice: {type: Number},
   vipWeeklyPrice: {type: Number},
+  vipMonthlyPrice: {type: Number},
   vipForeverPrice: {type: Number},
-
 });
 module.exports = model("default-settings", DefaultSettingsSchema);
