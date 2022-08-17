9f34f6b4017369f51b4558896cecb09857c6cd74
Akbar <akhusanbaev1@gmail.com>
2022-08-17 23:13:10 +0500

    make it better

--- index.js
+++ index.js
@@ -196,7 +196,7 @@ bot.on("message", async msg => {
     if (admin && admin.state.on === "reports-unban") return adminReportsUnbanPage(msg, admin);
     await Users.findOneAndUpdate({"user.id": user.user.id}, {lastAction: moment().toDate()});
     if (user.left) await Users.findOneAndUpdate({"user.id": user.user.id}, {left: false, backDate: moment().toDate()});
-    if (user.vip && user.trialSearches && user.trialSearches === 0 && !user.vipUntilDate && !user.vipUnlimited && user.state.on !== "chat" && user.state.on !== "ended-chat") {
+    if (user.vip && user.trialSearches === 0 && !user.vipUntilDate && !user.vipUnlimited && user.state.on !== "chat" && user.state.on !== "ended-chat") {
       console.log("DEBUGGING");
       user.vip = false;
       await Users.findOneAndUpdate({"user.id": user.user.id}, {vip: false});
